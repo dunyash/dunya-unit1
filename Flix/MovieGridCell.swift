7b81046655dfcb67b162f351de9c7f1bf6c62462
dunyash <89614617+dunyash@users.noreply.github.com>
2022-09-13 20:20:47 -0400

    Added details view, tab navigation, README updated

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Dunyagozel Durdyyeva on 9/7/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
